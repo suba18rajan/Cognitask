767f1c739899b65aec11e9e599b63d6337884bae
suba444raj@gmail.com <142592205+suba18rajan@users.noreply.github.com>
2026-09-19 20:57:17 +0530

    Set up EF Core and SQL Server database

---
+++ src/Cognitask.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Cognitask.Api.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Cognitask.Api.Data;
+
+public class ApplicationDbContext : DbContext
+{
+    public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+        : base(options)
+    {
+    }
+
+    public DbSet<User> Users => Set<User>();
+}
--- src/Cognitask.Api/Program.cs
+++ src/Cognitask.Api/Program.cs
@@ -1,6 +1,13 @@
+using Cognitask.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
