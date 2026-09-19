77dbd3ac80ff1f77e083089ace8df01df0a54cc2
suba444raj@gmail.com <142592205+suba18rajan@users.noreply.github.com>
2026-09-19 19:59:10 +0530

    Initial Cognitask API setup

---
+++ src/Cognitask.Api/Controller/HealthController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Cognitask.Api.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult Get()
+    {
+        return Ok(new
+        {
+            status = "Healthy",
+            application = "Cognitask API"
+        });
+    }
+}
